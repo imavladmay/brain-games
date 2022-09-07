ed1d17e604315046058b5a5bdbbf48cb712259af
Vladislav Mayakin <vlad99may@gmail.com>
2022-09-07 16:38:33 +0300

    Fix code in even.js

--- src/even.js
+++ src/even.js
@@ -2,33 +2,32 @@ import readlineSync from 'readline-sync';
 
 const evenGame = () => {
   console.log('Welcome to the Brain Games!');
+
   const userName = readlineSync.question('May I have your name? ');
+
   console.log(`Hello, ${userName}!`);
+  console.log('Answer "yes" if the number is even, otherwise answer "no".');
 
   const getRandomInt = (max) => Math.floor(Math.random() * max);
 
-  const beginEvenGame = () => {
-    console.log('Answer "yes" if the number is even, otherwise answer "no".');
+  for (let i = 0; i < 3; i += 1) {
+    const number = getRandomInt(100);
+    console.log('Question:', number);
+    const answerUser = readlineSync.question('Your answer: ');
 
-    for (let i = 0; i < 3; i += 1) {
-      const number = getRandomInt(100);
-      console.log('Question: ', number);
-      const answerUser = readlineSync.question('Your answer: ');
+    const firstChecking = (number % 2 === 0) && (answerUser === 'yes');
+    const secondChecking = (number % 2 !== 0) && (answerUser === 'no');
 
-      const firstChecking = (number % 2 === 0) && (answerUser === 'yes');
-      const secondChecking = (number % 2 !== 0) && (answerUser === 'no');
-
-      if (firstChecking === true || secondChecking === true) {
-        console.log('Correct!');
-      } else {
-        const oppositeAnswer = answerUser === 'yes' ? 'no' : 'yes';
-        const errorMessage = `'${answerUser}' is wrong answer ;(. Correct answer was '${oppositeAnswer}'./nLet's try again, ${userName}!`;
-        return errorMessage;
-      }
+    if (firstChecking === true || secondChecking === true) {
+      console.log('Correct!');
+    } else {
+      const oppositeAnswer = answerUser === 'yes' ? 'no' : 'yes';
+      const errorMessage = `'${answerUser}' is wrong answer ;(. Correct answer was '${oppositeAnswer}'.\nLet's try again, ${userName}!`;
+      return console.log(errorMessage);
     }
+  }
 
-    console.log(`Congratulations, ${userName}!`);
-  };
+  console.log(`Congratulations, ${userName}!`);
 };
 
 export default evenGame;
